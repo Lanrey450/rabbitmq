988d45c4c0d8ca8b52b4811ba1ba86462854e90e
Alexander Nnakwue <annakwue@terragonltd.com>
2020-02-28 10:47:39 +0100

    add mongo setup

--- mongoClient.js
+++ mongoClient.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 const debug = require('debug')('mongodb');
 const config = require('./config');
 
+console.log(config, 'config')
+
+
+
 const defaultConfig = {
-    username: config.user,
-    password: config.password,
-    host: config.host,
-    port: config.port,
-    db: config.db,
+    username: config.databases.mongodb.user,
+    password: config.databases.mongodb.password,
+    host: config.databases.mongodb.host,
+    port: config.databases.mongodb.port,
+    db: config.databases.mongodb.db,
 };
 
-const defaultUrl = `mongodb://${defaultConfig.username}:${defaultConfig.password}@${defaultConfig.host}:${defaultConfig.port}/${defaultConfig.db}` || 'mongodb://localhost:27017/subscription&billing';
+console.log(defaultConfig, "default config");
+
+const defaultUrl = `mongodb://${defaultConfig.username}:${defaultConfig.password}@${defaultConfig.host}:${defaultConfig.port}/${defaultConfig.db}?retryWrites=true` || 'mongodb://localhost:27017/subscription&billing?retryWrites=true';
 
 mongoose.set('debug', true);
 
@@ -24,7 +30,7 @@ db.on("connected", () => {
 });
 db.on('error', (error) => {
     console.error("An error occurred", JSON.stringify(error));
-    logger(error.message, new Error(error.message), { mongodbUrl }, true);
+    console.log(error.message, new Error(error.message), { defaultUrl }, true);
     process.exit(0);
 });
 
