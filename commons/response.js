27530b4852515c9fdc527a0c7801f1957d6632be
Alexander Nnakwue <annakwue@terragonltd.com>
2020-03-03 09:54:45 +0100

    update

--- commons/response.js
+++ commons/response.js
@@ -1,17 +1,26 @@
-module.exports = {
-    sendResponse({res, statusCode = 200, message = "success", responseBody}){
-        res.status(statusCode).send({
-            data: responseBody,
-            status: true,
-            message
-        });
-    },
-
-    sendErrorResponse({res, statusCode=500, message = "error", responseBody}){
-        res.status(statusCode).send({
-            data: responseBody,
-            status: false,
-            message
-        })
-    }
-}
+/**
+ * An express Middleware that attaches two new functions for responding with error/success messages
+ * @param req
+ * @param res
+ * @param next
+ */
+module.exports = (req, res, next) => {
+    res.successResponse = (({ statusCode = 200, message = 'success', data }) => {
+      res.status(statusCode).send({
+        data,
+        status: true,
+        message,
+      });
+    });
+  
+    res.errorResponse = (({ statusCode = 500, message = 'error', data }) => {
+      res.status(statusCode).send({
+        data,
+        status: true,
+        message,
+      });
+    });
+  
+    next();
+  };
+  
