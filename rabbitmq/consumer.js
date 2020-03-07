157a093509698e6605170110f52b41c6898bdaeb
abassade <abs4real16.ma@gmail.com>
2020-03-07 10:06:39 +0100

    implemented consumer for mtn

---
+++ rabbitmq/consumer.js
@@ -0,0 +1,23 @@
+/* eslint-disable eol-last */
+/* eslint-disable no-tabs */
+const amqp = require('amqplib')
+const config = require('../config')
+
+const consumeFromQueue = async (queue, callback) => {
+	try {
+		const cluster = await amqp.connect(`amqp://${config.rabbit_mq.user}:${config.rabbit_mq.pass}@${config.rabbit_mq.host}:${config.rabbit_mq.port}/${config.rabbit_mq.vhost}`)
+		const channel = await cluster.createChannel()
+		await channel.assertQueue(queue, { durable: true, noAck: false })
+		await channel.consume(queue, (msg)=> {
+                channel.ack(msg);
+                callback(null, msg.content.toString());
+        });
+		await channel.close()
+	} catch (error) {
+        // handle error response
+        callback(error, null);
+		console.error(error, 'Unable to connect to cluster!')
+		process.exit(1)
+	}
+}
+module.exports = consumeFromQueue
