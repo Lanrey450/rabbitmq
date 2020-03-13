2d3dd8644e15731369e5ba4039d59f26ac517e27
Alexander Nnakwue <annakwue@terragonltd.com>
2020-03-13 14:54:29 +0100

    minor fixs

--- routes/subscription.js
+++ routes/subscription.js
@@ -1,11 +1,8 @@
 /* eslint-disable no-tabs */
 /* eslint-disable camelcase */
 const express = require('express')
-
 const route = express.Router()
 const asyncHandler = require('express-async-handler')
-
-
 const NineMOBILESubscriptionController = require('../controllers/9Mobile/subscription')
 const AirtelSubscriptionController = require('../controllers/airtel/subscription')
 const MTNSubscriptionController = require('../controllers/mtn/subscription')
@@ -19,17 +16,21 @@ route.post('/nineMobile/subscribe', asyncHandler((req, res) => NineMOBILESubscri
 route.post('/nineMobile/unsubscribe', asyncHandler((req, res) => NineMOBILESubscriptionController.unsubscribe(req, res)))
 route.get('/nineMobile/status', asyncHandler((req, res) => NineMOBILESubscriptionController.status(req, res)))
 
-route.post('/nineMobile/sms/mo', asyncHandler((req, res) => NineMOBILE_MO_Controller.userConsent(req, res)))
-
-// Nine mobile postback
-route.post('/nineMobile/subscription/optout', asyncHandler((req, res) => NineMobilePostbackController.optoutRequest(req, res)))
-route.post('/nineMobile/charge', asyncHandler((req, res) => NineMobilePostbackController.chargeAsyncRequest(req, res)))
-route.post('/nineMobile/subscription/optin', asyncHandler((req, res) => NineMobilePostbackController.optinRequest(req, res)))
-route.post('/nineMobile/consent', asyncHandler((req, res) => NineMobilePostbackController.consentmoRequest(req, res)))
 
 // billing sync and async
 route.post('/nineMobile/billing/sync', asyncHandler((req, res) => NinemobileChargeController.chargeSync(req, res)))
 route.post('/nineMobile/billing/async', asyncHandler((req, res) => NinemobileChargeController.chargeAsync(req, res)))
+
+
+// Nine mobile SMS-MO(INCOMING FROM TELCO)
+route.post('/nineMobile/sms/mo', asyncHandler((req, res) => NineMOBILE_MO_Controller.userConsent(req, res)))
+
+
+// Nine mobile postback (INCOMING FROM TELCO)
+route.post('/nineMobile/subscription/optout', asyncHandler((req, res) => NineMobilePostbackController.optoutRequest(req, res)))
+route.post('/nineMobile/charge/async', asyncHandler((req, res) => NineMobilePostbackController.chargeAsyncRequest(req, res)))
+route.post('/nineMobile/subscription/optin', asyncHandler((req, res) => NineMobilePostbackController.optinRequest(req, res)))
+route.post('/nineMobile/consent', asyncHandler((req, res) => NineMobilePostbackController.consentmoRequest(req, res)))
 
 
 // postback 9Mobile (called by telco)
@@ -39,18 +40,19 @@ route.post('/nineMobile/billing/feedback', asyncHandler((req, res) => NineMOBILE
 // Airtel sub
 route.post('/airtel/subscribe', asyncHandler((req, res) => AirtelSubscriptionController.subscribeRequest(req, res)))
 route.post('/airtel/unsubscribe', asyncHandler((req, res) => AirtelSubscriptionController.unSubscribeRequest(req, res)))
-route.get('/airtel/status', asyncHandler((req, res) => AirtelSubscriptionController.getSubscriptionStatus(req, res)))
+route.post('/airtel/status', asyncHandler((req, res) => AirtelSubscriptionController.getSubscriptionStatus(req, res)))
 
-// Airtel postback
+
+// Airtel postback (INCOMING FROM TELCO)
 route.post('/airtelPostBack', asyncHandler((req, res) => AirtelSubscriptionController.airtelDataSyncPostBack(req, res)))
 
 // MTN sub
 route.post('/mtn/subscribe', asyncHandler((req, res) => MTNSubscriptionController.subscribe(req, res)))
 route.post('/mtn/unsubscribe', asyncHandler((req, res) => MTNSubscriptionController.unsubscribe(req, res)))
-route.get('/mtn/status', asyncHandler((req, res) => MTNSubscriptionController.status(req, res)))
+route.post('/mtn/status', asyncHandler((req, res) => MTNSubscriptionController.status(req, res)))
 
 
-// // mtn postback
+// mtn postback (INCOMING FROM TELCO)
 route.post('/mtnPostBack', asyncHandler((req, res) => MTNSubscriptionController.MTNDataSyncPostBack(req, res)))
 
 
