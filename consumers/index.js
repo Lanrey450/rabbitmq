2806ced7342e8a894507f8839c958587d0e1ab9a
Alexander Nnakwue <annakwue@terragonltd.com>
2020-05-09 23:25:48 +0100

    remove type field and modify function since type is already sent to the queue

--- consumers/index.js
+++ consumers/index.js
@@ -27,64 +27,55 @@ module.exports = {
 	saveConsumedSubscriptionDataMTN() {
 		const feedbackQueue = config.feedbackQueues.SubscriptionFeedbackQUEUE
 		const queue = config.rabbit_mq.mtn.subscription_queue
-		const type = 'MTN'
-		consumeHandler(feedbackQueue, queue, MtnSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, MtnSubscriptionModel)
 	},
 	saveConsumedUnSubscriptionDataMTN() {
 		const feedbackQueue = config.feedbackQueues.UnsubscriptionFeedbackQUEUE
-		const type = 'MTN'
 		const queue = config.rabbit_mq.mtn.un_subscription_queue
-		consumeHandler(feedbackQueue, queue, MtnSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, MtnSubscriptionModel)
 	},
 	saveConsumedPostbackDataMTN() {
 		const feedbackQueue = config.feedbackQueues.BillingFeedbackQUEUE
-		const type = 'MTN'
 		const queue = config.rabbit_mq.mtn.postback_queue
-		consumeHandler(feedbackQueue, queue, MtnSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, MtnSubscriptionModel)
 	},
 
 	// AIRTEL CONSUMERS
 	saveConsumedSubscriptionDataAIRTEL() {
 		const feedbackQueue = config.feedbackQueues.SubscriptionFeedbackQUEUE
 		const queue = config.rabbit_mq.airtel.subscription_queue
-		const type = 'AIRTEL'
-		consumeHandler(feedbackQueue, queue, AirtelSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, AirtelSubscriptionModel)
 	},
 	saveConsumedUnsubscriptionDataAIRTEL() {
 		const feedbackQueue = config.feedbackQueues.UnsubscriptionFeedbackQUEUE
 		const queue = config.rabbit_mq.airtel.un_subscription_queue
-		const type = 'AIRTEL'
-		consumeHandler(feedbackQueue, queue, AirtelSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, AirtelSubscriptionModel)
 	},
 	saveConsumedPostbackDataAIRTEL() {
 		const feedbackQueue = config.feedbackQueues.BillingFeedbackQUEUE
 		const queue = config.rabbit_mq.airtel.postback_queue
-		const type = 'AIRTEL'
-		consumeHandler(feedbackQueue, queue, AirtelPostbackModel, type)
+		consumeHandler(feedbackQueue, queue, AirtelPostbackModel)
 	},
 
 	// NINE MOBILE CONSUMERS
 	saveConsumedSubscriptionData9Mobile() {
 		const feedbackQueue = config.feedbackQueues.SubscriptionFeedbackQUEUE
 		const queue = config.rabbit_mq.nineMobile.subscription_queue
-		const type = '9MOBILE'
-		consumeHandler(feedbackQueue, queue, NineMobileSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, NineMobileSubscriptionModel)
 	},
 	saveConsumedUnsubscriptionData9Mobile() {
 		const feedbackQueue = config.feedbackQueues.UnsubscriptionFeedbackQUEUE
 		const queue = config.rabbit_mq.nineMobile.un_subscription_queue
-		const type = '9MOBILE'
-		consumeHandler(feedbackQueue, queue, NineMobileSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, NineMobileSubscriptionModel)
 	},
 	saveConsumedPostbackData9Mobile() {
 		const feedbackQueue = config.feedbackQueues.BillingFeedbackQUEUE
 		const queue = config.rabbit_mq.nineMobile.postback_queue
-		const type = '9MOBILE'
-		consumeHandler(feedbackQueue, queue, NineMobileSubscriptionModel, type)
+		consumeHandler(feedbackQueue, queue, NineMobileSubscriptionModel)
 	},
 }
 
-function consumeHandler(feedbackQueue, consumerQueue, model, _type = '') {
+function consumeHandler(feedbackQueue, consumerQueue, model) {
 	consume(consumerQueue, async (err, msg) => {
 		TerraLogger.debug('!!!!!!!reaching consumer engine...!!!!!!!!!')
 		if (err) {
@@ -97,11 +88,9 @@ function consumeHandler(feedbackQueue, consumerQueue, model, _type = '') {
 		}
 		if (msg != null && feedbackQueue != null) {
 			try {
-				msg.telcoType = _type
 				await publish(feedbackQueue, msg)
 				TerraLogger.debug('Successfully pushed to feedback queue')
 				msg.feedbackStatus = true
-				delete msg.telcoType
 				try {
 					TerraLogger.debug(msg)
 					const data = await model.create(msg)
@@ -111,8 +100,9 @@ function consumeHandler(feedbackQueue, consumerQueue, model, _type = '') {
 					TerraLogger.debug(`unable to save data to mongodb - ${error}`)
 				}
 			} catch (feedbackPublishError) {
-				TerraLogger.debug(`unable to push feedback to queue${feedbackPublishError}`)
+				TerraLogger.debug(`unable to push feedback to queue - ${feedbackPublishError}`)
 				msg.feedbackStatus = false
+				// save to databse regardless
 				try {
 					const data = await model.create(msg)
 					if (data) {
@@ -125,6 +115,7 @@ function consumeHandler(feedbackQueue, consumerQueue, model, _type = '') {
 			}
 			return
 		}
+
 		if (msg != null && feedbackQueue == null) {
 			try {
 				msg.feedbackStatus = false
