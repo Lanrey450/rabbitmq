9b1a8dd276075237d3bfd06e62559a47ccc9bcf3
Terra-Alex <terra-alex.@Alexs-MacBook-Pro.local>
2021-01-21 13:13:34 +0100

    change to uppercase

--- controllers/9Mobile/subscription.js
+++ controllers/9Mobile/subscription.js
@@ -97,7 +97,7 @@ module.exports = {
 				try {
 					const nineMobileReqBody = {
 						userIdentifier: req.body.msisdn,
-						entryChannel: req.body.channel,
+						entryChannel: req.body.channel.toUpperCase(),
 						serviceId: req.body.serviceId,
 					}
 
