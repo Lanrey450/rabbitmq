537c1d29d22b3c8d0f0be574aa4cb4bd7838ff9e
Kazeem <kjimoh@terragonltd.com>
2021-02-22 16:45:33 +0100

    AG-61-9mobile-uat updates

--- controllers/9Mobile/postbackHandler.js
+++ controllers/9Mobile/postbackHandler.js
@@ -181,22 +181,6 @@ module.exports = {
 		
 		try {
 
-			const planDetails = await axios.get(`http://staging-api-gateway.terragonbase.com/products/api/v1/plans/query?serviceId=${data.serviceId}&network=9mobile`, {
-				headers: {
-					'Authorization': "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjp7ImVtYWlsVmVyaWZpZWRBdCI6bnVsbCwicGhvbmVWZXJpZmllZEF0IjpudWxsLCJ1c2VySWQiOiI1ZGY3ODQzMThhNDdlNDAwMWJlZTU4MjciLCJyb2xlIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluQHRlbGVjb21tZS5jb20iLCJub3RpZmljYXRpb25MYXN0UmVhZEF0IjoiMjAyMC0wMy0xMlQxMjoyMzo0NC4yMTRaIiwiYXV0aElkIjoiNWRmNzg0MzI4YTQ3ZTQwMDFiZWU1ODJhIiwicHJvZmlsZSI6eyJuYW1lIjoiVGVsZWNvbW1lIEFkbWluIiwiZW1haWwiOiJhZG1pbkB0ZWxlY29tbWUuY29tIiwicGhvbmVOdW1iZXIiOiIrMjM0NzAxMjM0NTY3OCIsInVzZXJJZCI6IjVkZjc4NDMxOGE0N2U0MDAxYmVlNTgyNyJ9LCJub3RpZmljYXRpb25zIjpbeyJkZXNjcmlwdGlvbiI6IkFjY291bnQgQXBwcm92YWxzOiAyIHByb3ZpZGVyKHMpIGFyZSBhd2FpdGluZyBhcHByb3ZhbCIsImxldmVsIjoid2FybmluZyJ9XSwidW5yZWFkIjp7InRpY2tldHMiOjAsInByb2R1Y3RzIjoxNTR9fSwiaWF0IjoxNTg1Mzg0Nzg2LCJhdWQiOiJ2YXMtYWdnIiwiaXNzIjoidmFzLWF1dGgiLCJzdWIiOiJ2YXMtYWdnIn0.T5H42csX4L4rVM9Om9fr_fNulZU8vSCeC-jif-tm-XS96Z5Ncq0Npv_fHnw78S5Uq6uJW9gK-AMfvPFfkWBsZg"
-				}
-			});
-
-			console.log('PLan', planDetails.data.data);
-
-			const { plan, name } = planDetails.data.data;
-
-			// const messagePayload  = { msisdn: data.userIdentifier, name, amount: plan.amount, validity: plan.validity, shortCode };
-
-			// console.log('message body', messagePayload);
-			// NineMobileUtils.sendUserWelcomeSMSforUSSD(messagePayload).then(TerraLogger.debug).catch(TerraLogger.debug)
-
-
 			publish(config.rabbit_mq.vasQueues.CONSENT_BILLING, { ...dataToPush }) // subscription feedback queue
 				.then(() => {
 					console.log('successfully pushed to the 9mobile postback queue')
