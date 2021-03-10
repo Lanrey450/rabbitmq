55449c5ea8ce7633697a4ec8232d14cbe82a682d
Kazeem <kjimoh@terragonltd.com>
2021-03-10 08:34:19 +0100

    updates

--- controllers/9Mobile/subscription.js
+++ controllers/9Mobile/subscription.js
@@ -112,7 +112,7 @@ module.exports = {
 
 			if (username === config.userAuth.username && rawPassword === config.userAuth.password) {
 				//------HACK--------
-				const { name, shortCode, keyword } = req.body;
+				const { name, shortCode, keyword, serviceId } = req.body;
 				const redisSubscriptionKey = `UNSUBSCRIPTION_CALL::${serviceId}::${msisdn}`;
 				console.log("ussd subscription call key " + redisSubscriptionKey)
 				redis.set(redisSubscriptionKey, `${name}::${shortCode}::${keyword}`, 'ex', 60 * 60) // save for 1 hour
